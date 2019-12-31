02c342dd01d08a664de28c691a2ddf054aafd771
dingzan <1105503089@qq.com>
2019-12-31 09:24:39 +0800

    fix: "更新事件发布订阅组件"

--- src/components/address-book/address-book.vue
+++ src/components/address-book/address-book.vue
@@ -2,6 +2,7 @@
   <div class="address-book">
     address-book
     <img :src="logo" alt />
+    <el-button @click="clickMethod()"></el-button>
   </div>
 </template>
 
@@ -10,6 +11,8 @@ import { Vue, Component } from "vue-property-decorator";
 import { Getter } from "vuex-class";
 import { Apptypes } from "../../store/types/app-types";
 import addressBookService from "../../api/address-book-service";
+import { rxevent } from 'justin-event-bus';
+import { EventKeys } from "../../common/constant/event-keys";
 
 @Component({})
 export default class AddressBook extends Vue {
@@ -27,6 +30,11 @@ export default class AddressBook extends Vue {
       .catch(() => {
         console.log("-------------报错啦");
       });
+  }
+
+  clickMethod() {
+    console.log(";;;;;;;;;");
+    rxevent.publish(EventKeys.Justin, "2312312312");
   }
 }
 </script>
--- src/views/content-fixed/content-fixed.vue
+++ src/views/content-fixed/content-fixed.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="content-fixed">
-    content-fix
-  </div>
+  <div class="content-fixed">content-fix</div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
+import { rxevent } from "justin-event-bus";
+import { EventKeys } from "../../common/constant/event-keys";
 
 @Component({})
 export default class ContentFixed extends Vue {
+  created() {
+    rxevent.subscribe(EventKeys.Justin, "ContentFixed", (item: any) => {
+      console.log("-------------------item", item);
+    });
+  }
 }
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-@import './content-fixed.scss'
+@import "./content-fixed.scss";
 </style>
