230185b6ce7d26514a3374038c64d80e196ebc10
dingzan <1105503089@qq.com>
2020-01-08 17:25:18 +0800

    fix: "上传新的HTTP请求包依赖"

--- src/api/address-book-service.ts
+++ src/api/address-book-service.ts
@@ -1,6 +1,6 @@
 import store from '@/store';
 import Axios from 'axios';
-import * as httpclient from 'justin-http/src/http-client/httpclient'
+import * as httpclient from 'justin-http/dist/httpclient'
 
 /**
  * 通讯录服务
@@ -12,6 +12,6 @@ export default {
     async getAddressBook(): Promise<any> {
         const url = `${store.getters.configs.mockUrl}` + 'getUserInfo'
         // return await Axios.get(url)
-        return await httpclient.getPromise(url);
+        return await httpclient.getPromise(url)
     }
 }
--- src/components/address-book/address-book.vue
+++ src/components/address-book/address-book.vue
@@ -3,6 +3,7 @@
     address-book
     <img :src="logo" alt />
     <el-button @click="clickMethod()"></el-button>
+    <p>{{date}}</p>
   </div>
 </template>
 
@@ -11,17 +12,21 @@ import { Vue, Component } from "vue-property-decorator";
 import { Getter } from "vuex-class";
 import { Apptypes } from "../../store/types/app-types";
 import addressBookService from "../../api/address-book-service";
-import { rxevent } from 'justin-event-bus';
+import { rxevent } from "justin-event-bus";
 import { EventKeys } from "../../common/constant/event-keys";
+import { format } from "justin-package-gutils/dist/util-helper";
 
 @Component({})
 export default class AddressBook extends Vue {
   @Getter(Apptypes.getters.CONFIGS)
   configs: any;
   private logo = require("../../assets/images/logo.png");
+  private date = "";
 
   created() {
     console.log("-------------", this.configs);
+    this.date = format(new Date(), "yyyy-MM-dd HH:mm:ss");
+
     addressBookService
       .getAddressBook()
       .then((res: any) => {
