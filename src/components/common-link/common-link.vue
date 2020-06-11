96720236cb63167c285fa3c3cefa3dbfb436d0aa
dingzan <1105503089@qq.com>
2020-06-11 16:27:47 +0800

    fix: "上传模块"

--- src/components/common-link/common-link.vue
+++ src/components/common-link/common-link.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="common-link">
-    common-link
+    <div class="top-box">top-box</div>
+    <div class="bottom-box">
+      <div :class="['show-box',{'box-show':boxsShow}]">bottom-box</div>
+      <p class="button" @click="clickMothed()">button</p>
+    </div>
   </div>
 </template>
 
@@ -9,9 +13,13 @@ import { Vue, Component } from "vue-property-decorator";
 
 @Component({})
 export default class CommonLink extends Vue {
+  private boxsShow = false;
+  clickMothed() {
+    this.boxsShow = !this.boxsShow;
+  }
 }
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-@import './common-link.scss'
+@import "./common-link.scss";
 </style>
